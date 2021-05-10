88d4c0aa06688056398da330320e2027c598f410
Rick Brown <ricky.brown.00@gmail.com>
2021-05-10 13:21:30 +0100

    formats whitespace

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -2,8 +2,14 @@ export default function Blog({ offsetY }) {
   console.log(offsetY)
   return (
     <>
-      <div className="blog-background-image" style={{ transform: `translateY(${offsetY * 0.2}px)`}}/>
-      <div className="blog-moving-part" style={{ transform: `translateY(${offsetY / 0.4}px)`}}/>
+      <div
+        className="blog-background-image"
+        style={{ transform: `translateY(${offsetY * 0.2}px)` }}
+      />
+      <div
+        className="blog-moving-part"
+        style={{ transform: `translateY(${offsetY * 2}px)` }}
+      />
     </>
   )
 }
--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -3,11 +3,13 @@ import AnimatedWord from './AnimatedComponents/AnimatedWord'
 
 export default function MainSection({ offsetY }) {
   return (
-    <div className="main-section-container" style={{ transform: `translate(${offsetY * 1.6}px, ${offsetY}px)`}}>
-        <h1>Hi, my name is</h1>
-        <AnimatedWord word={"Rick Brown"} />
-        <h1>{"& I love to create unique user experiences"}</h1>
-        <h1>{"for both the web and iOS devices."}</h1>
+    <div
+      className="main-section-container"
+      style={{ transform: `translate(${offsetY * 3}px, ${offsetY}px)` }}>
+      <h1>Hi, my name is</h1>
+      <AnimatedWord word={'Rick Brown'} />
+      <h1>{'& I love to create unique user experiences'}</h1>
+      <h1>{'for both the web and iOS devices.'}</h1>
     </div>
   )
 }
