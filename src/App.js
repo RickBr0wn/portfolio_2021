3ad866a14fb2d610479a1048a6629c81f9cba73a
Rick Brown <ricky.brown.00@gmail.com>
2021-05-10 12:44:42 +0100

    adds App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from 'react'
 import Header from './Components/Header'
 import MainSection from './Components/MainSection'
 import Footer from './Components/Footer'
+import Blog from './Components/Blog'
 
 function App() {
+  const [offsetY, setOffsetY] = useState(0)
+  const handleScroll = () => {
+    console.log(window.pageYOffset)
+    setOffsetY(window.pageYOffset)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
   return (
     <>
-      <Header />
-      <MainSection />
-      <Footer />
+      <div className="parent">
+        <Header />
+      </div>
+      <MainSection offsetY={offsetY} />
+      <Blog offsetY={offsetY} />
+      <div className="parent">
+        <Footer />
+      </div>
     </>
   )
 }
