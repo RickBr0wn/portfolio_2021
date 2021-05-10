99edbb3704935fd89dca45356cdb26d0f85950b2
Rick Brown <ricky.brown.00@gmail.com>
2021-05-10 12:45:15 +0100

    adds MainSection

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import AnimatedWord from './AnimatedComponents/AnimatedWord'
 
-export default function MainSection() {
-  
-
+export default function MainSection({ offsetY }) {
   return (
-    <div className="main-section-container">
+    <div className="main-section-container" style={{ transform: `translate(${offsetY * 1.6}px, ${offsetY}px)`}}>
         <h1>Hi, my name is</h1>
         <AnimatedWord word={"Rick Brown"} />
         <h1>{"& I love to create unique user experiences"}</h1>
